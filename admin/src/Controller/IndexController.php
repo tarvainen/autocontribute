9f3f31527cd8ced0aac3ec155fe1a1b4993fea0f
tarvainen <atte.tarvainen@protacon.com>
2019-02-02 15:56:02 +0200

    Add CRUD for Task
    Together that add simple UI using Bulma and Symfony's templates

---
+++ admin/src/Controller/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class IndexController
+ *
+ * @package App\Controller
+ * @author Atte Tarvainen <tarvainen.atte@gmail.com>
+ */
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/index", name="index")
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig');
+    }
+}
---
+++ admin/src/Form/TaskType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class TaskType
+ *
+ * @package App\Form
+ * @author Atte Tarvainen <tarvainen.atte@gmail.com>
+ */
+class TaskType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('description')
+            ->add('action')
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
